3629fb5be551322fde8ec96b8a6ca93e2b8a4d3d
GastonTittarelli <116087636+GastonTittarelli@users.noreply.github.com>
2024-07-30 16:30:20 -0300

    style (large mobile): large mobile mediaqueries 15% of progres

--- src/assets/components/Main/backroundImg/BackgroundIm.tsx
+++ src/assets/components/Main/backroundImg/BackgroundIm.tsx
@@ -5,7 +5,7 @@ const BackgroundIm = () => {
         <div className={styles.container}>
 
             <div className={styles.backroundImg}>
-                    <div className={styles.textHeader}>
+                <div className={styles.textHeader}>
                     <h1>LuigiaArt</h1>
                     <h2>Bienvenidos a mi Espacio Artístico</h2>
                     <p>Aquí podrás explorar las Emociones Capturadas en mis Creaciones</p>
