88fc3962ea29e8c38aa016531056aaf2b0bf0f88
GastonTittarelli <116087636+GastonTittarelli@users.noreply.github.com>
2024-08-09 12:29:03 -0300

    fix (destacados view): destacados view now is aviable on safari and mozilla browsers

--- src/assets/components/Main/destacado/ImagenList3.tsx
+++ src/assets/components/Main/destacado/ImagenList3.tsx
@@ -19,17 +19,18 @@ export const getItemsByCategory = async (categoria: string): Promise<Item[]> => 
     return itemsdata;
 };
 
-export const ItemsByCategory = () => {
+export const ItemsByCategory1 = () => {
     const [itemsPintura, setItemsPintura] = useState<Item[]>([]);
     const [itemsRestauracion, setItemsRestauracion] = useState<Item[]>([]);
     const [itemsCreacion, setItemsCreacion] = useState<Item[]>([]);
-    const [imagesLoaded, setImagesLoaded] = useState(false);
+    const [imagesLoaded, setImagesLoaded] = useState({ pintura: false, restauracion: false, creacion: false });
 
     useEffect(() => {
         const fetchItemsByCategory = async () => {
             const itemsPinturaData = await getItemsByCategory("pintura");
             const itemsRestauracionData = await getItemsByCategory("restauracion");
             const itemsCreacionData = await getItemsByCategory("creacion");
+
             setItemsPintura(itemsPinturaData);
             setItemsRestauracion(itemsRestauracionData);
             setItemsCreacion(itemsCreacionData);
@@ -37,104 +38,67 @@ export const ItemsByCategory = () => {
         fetchItemsByCategory();
     }, []);
 
-    useEffect(() => {
-        if (itemsPintura.length > 0 && itemsRestauracion.length > 0 && itemsCreacion.length > 0) {
-            setImagesLoaded(false);
-            const images = document.querySelectorAll<HTMLImageElement>(`.${styles.imagenes}`);
-            let loadedCount = 0;
+    const handleImageLoad = (category: keyof typeof imagesLoaded) => {
+        setImagesLoaded(prevState => ({
+            ...prevState,
+            [category]: true,
+        }));
+    };
 
-            const handleImageLoad = () => {
-                loadedCount++;
-                if (loadedCount === images.length) {
-                    setImagesLoaded(true);
-                }
-            };
-
-            images.forEach((img) => {
-                if (img.complete) {
-                    handleImageLoad();
-                } else {
-                    img.addEventListener('load', handleImageLoad);
-                }
-            });
-
-            return () => {
-                images.forEach((img) => {
-                    img.removeEventListener('load', handleImageLoad);
-                });
-            };
-        }
-    }, [itemsPintura, itemsRestauracion, itemsCreacion]);
+    const renderImages = (items: Item[], category: keyof typeof imagesLoaded) => (
+        <>
+            <section className={`${styles.imagenesSide} ${imagesLoaded[category] ? styles.loaded : ''}`}>
+                {items.map((item) => (
+                    <div className={styles.contenedorImg} key={item.id}>
+                        <Link to={`${item.id}`}>
+                            <img
+                                className={styles.imagenes}
+                                loading="lazy"
+                                src={item.subCategoria2}
+                                alt={item.titulo}
+                                onLoad={() => handleImageLoad(category)}
+                                onError={() => handleImageLoad(category)} // También manejamos errores
+                            />
+                        </Link>
+                    </div>
+                ))}
+            </section>
+            <section className={`${styles.imagenesSide} ${imagesLoaded[category] ? styles.loaded : ''}`}>
+                {items.map((item) => (
+                    <div className={styles.contenedorImg} key={item.id}>
+                        <Link to={`${item.id}`}>
+                            <img
+                                className={styles.imagenes}
+                                loading="lazy"
+                                src={item.subCategoria2}
+                                alt={item.titulo}
+                                onLoad={() => handleImageLoad(category)}
+                                onError={() => handleImageLoad(category)}
+                            />
+                        </Link>
+                    </div>
+                ))}
+            </section>
+        </>
+    );
 
     return (
         <div className={styles.container}>
             <h2>Principales Destacados</h2>
 
             <div className={styles.pinturas}>
-                <section className={`${styles.imagenesSide} ${imagesLoaded ? styles.loaded : ''}`}>
-                    {itemsPintura.map((item) => (
-                        <div className={styles.contenedorImg} key={item.id}>
-                            <Link to={`${item.id}`}>
-                                <img className={styles.imagenes} loading="lazy" src={item.subCategoria2} alt={item.titulo} />
-                            </Link>
-                        </div>
-                    ))}
-                </section>
-                <section className={`${styles.imagenesSide} ${imagesLoaded ? styles.loaded : ''}`}>
-                    {itemsPintura.map((item) => (
-                        <div className={styles.contenedorImg} key={item.id}>
-                            <Link to={`${item.id}`}>
-                                <img className={styles.imagenes} loading="lazy" src={item.subCategoria2} alt={item.titulo} />
-                            </Link>
-                        </div>
-                    ))}
-                </section>
+                {renderImages(itemsPintura, 'pintura')}
             </div>
 
             <div className={styles.creacion}>
-                <section className={`${styles.imagenesSide} ${imagesLoaded ? styles.loaded : ''}`}>
-                    {itemsCreacion.map((item) => (
-                        <div className={styles.contenedorImg} key={item.id}>
-                            <Link to={`${item.id}`}>
-                                <img className={styles.imagenes} loading="lazy" src={item.subCategoria2} alt={item.titulo} />
-                            </Link>
-                        </div>
-                    ))}
-                </section>
-                <section className={`${styles.imagenesSide} ${imagesLoaded ? styles.loaded : ''}`}>
-                    {itemsCreacion.map((item) => (
-                        <div className={styles.contenedorImg} key={item.id}>
-                            <Link to={`${item.id}`}>
-                                <img className={styles.imagenes} loading="lazy" src={item.subCategoria2} alt={item.titulo} />
-                            </Link>
-                        </div>
-                    ))}
-                </section>
+                {renderImages(itemsCreacion, 'creacion')}
             </div>
 
             <div className={styles.restauracion}>
-                <section className={`${styles.imagenesSide} ${imagesLoaded ? styles.loaded : ''}`}>
-                    {itemsRestauracion.map((item) => (
-                        <div className={styles.contenedorImg} key={item.id}>
-                            <Link to={`${item.id}`}>
-                                <img className={styles.imagenes} loading="lazy" src={item.subCategoria2} alt={item.titulo} />
-                            </Link>
-                        </div>
-                    ))}
-                </section>
-                <section className={`${styles.imagenesSide} ${imagesLoaded ? styles.loaded : ''}`}>
-                    {itemsRestauracion.map((item) => (
-                        <div className={styles.contenedorImg} key={item.id}>
-                            <Link to={`${item.id}`}>
-                                <img className={styles.imagenes} loading="lazy" src={item.subCategoria2} alt={item.titulo} />
-                            </Link>
-                        </div>
-                    ))}
-                </section>
+                {renderImages(itemsRestauracion, 'restauracion')}
             </div>
         </div>
     );
 };
 
-export default ItemsByCategory;
-
+export default ItemsByCategory1;
