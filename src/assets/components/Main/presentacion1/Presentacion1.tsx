43e7f0905926335dc3a1e3fed3864b77d9e2a35e
GastonTittarelli <116087636+GastonTittarelli@users.noreply.github.com>
2024-08-14 16:22:34 -0300

    description in presentacion1 uptdated

--- src/assets/components/Main/presentacion1/Presentacion1.tsx
+++ src/assets/components/Main/presentacion1/Presentacion1.tsx
@@ -7,7 +7,7 @@ const Presentacion1 = () => {
                 <h2>Soy Marilú Borda</h2>
 
                 <div className={styles.pContainer}>
-                    <p>Mi espacio de arte que tanta felicidad me da. Es un lugar donde puedo ser libre, dejar volar mi imaginación y explorar un mundo lleno de posibilidades. A medida que me adentré en este terreno inexplorado, que siempre admiré desde la distancia, descubrí la valentía para experimentar y plasmar mi visión artística. La música, otra de mis grandes pasiones, no es solo un acompañamiento; es el latido que marca el ritmo de mi vida, inspirándome y enriqueciéndome en cada paso de mi exploración artística.</p> 
+                    <p>Mi espacio de arte que tanta felicidad me da. Es un lugar donde puedo ser libre, dejar volar mi imaginación y explorar un mundo lleno de posibilidades. A medida que me adentré en este terreno inexplorado, que siempre admiré desde la distancia, descubrí la valentía para experimentar y plasmar mi visión creativa. La música, otra de mis grandes pasiones, no es solo un acompañamiento; es el latido que marca el ritmo de mi vida, inspirándome y enriqueciéndome en cada paso de mi exploración artística.</p> 
 
                     <p>Este espacio lleva mi nombre y el de mis ancestros italianos, mujeres extraordinarias que fueron luz en mi camino, abriendo puertas y permitiéndome ser quien soy hoy.</p>
                 </div>
